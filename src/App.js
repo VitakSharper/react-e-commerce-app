d518410a1d2c2e92433cded94430a37e82c95675
VitakSharper <v.banard@gmail.com>
2019-11-30 17:15:41 +0100

    add homepage & styling with sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HomePage from "./homepage.component";
 
 function App() {
     return (
-        <div className="App">
-
+        <div>
+            <h1>My App</h1>
+            <HomePage/>
         </div>
     );
 }
