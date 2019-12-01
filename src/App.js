373691492fe0b97097c2919231a5239109a15b42
VitakSharper <v.banard@gmail.com>
2019-12-01 23:02:56 +0100

    added redux, and implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import React from 'react';
 import {Switch, Route, Link} from 'react-router-dom'
+import {connect} from 'react-redux';
 import './App.scss';
 import HomePage from "./pages/home/home.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+import {setCurrentUser} from './redux/user/user.actions';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentUser: null
-        }
-    }
 
     unsubscribeFromAuth = null;
 
     componentDidMount() {
+        const {setCurrentUser} = this.props;
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
 
                 userRef.onSnapshot(snapshot => {
-                    this.setState({
+                    setCurrentUser({
                         currentUser: {
                             id: snapshot.id,
                             ...snapshot.data()
@@ -31,7 +28,7 @@ class App extends React.Component {
                     });
                 });
             } else {
-                this.setState({currentUser: userAuth})
+                setCurrentUser(userAuth);
             }
         })
     }
@@ -43,9 +40,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header
-                    currentUser={this.state.currentUser}
-                />
+                <Header/>
                 <Switch>
                     <Route path='/' component={HomePage} exact/>
                     <Route path='/shop' component={ShopPage}/>
@@ -56,4 +51,8 @@ class App extends React.Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
