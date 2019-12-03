bb70a27f70f3cc6fc3a49f40ab7dca5766a6a10f
VitakSharper <v.banard@gmail.com>
2019-12-03 12:48:54 +0100

    improuve the cart-dropdown flow, close automaticaly when go to checkoutpage

--- src/components/cart/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,12 @@ import CartItem from "../../cart-item/cart-item.component";
 
 import {withRouter} from 'react-router-dom';
 
+import {toggleCartHidden} from '../../../redux/cart/cart.actions'
+
 import {createStructuredSelector} from "reselect";
 import {selectCartItems} from "../../../redux/cart/cart.selectors";
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             {
@@ -24,9 +26,13 @@ const CartDropdown = ({cartItems, history}) => (
                     <span className='empty-message'>Your cart is empty</span>
             }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            history.push('/checkout');
+            dispatch(toggleCartHidden())
+        }}>GO TO CHECKOUT</CustomButton>
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({cartItems: selectCartItems});
+
 export default withRouter(connect(mapStateToProps)(CartDropdown));
