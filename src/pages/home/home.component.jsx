5a08fa53ab2ab9cf41c2e0c56dbe58aa3202739c
VitakSharper <v.banard@gmail.com>
2019-12-30 18:43:30 +0100

    add react profiler

--- src/pages/home/home.component.jsx
+++ src/pages/home/home.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Profiler} from 'react';
 import Directory from "../../components/direcotry/direcotory.component";
 
 import {HomePageContainer} from "./home.styles";
@@ -6,7 +6,11 @@ import {HomePageContainer} from "./home.styles";
 const HomePage = () => {
     return (
         <HomePageContainer>
-            <Directory/>
+            <Profiler id={'Directory'} onRender={(id, phase, actualDuration) => {
+                console.log({id, phase, actualDuration})
+            }}>
+                <Directory/>
+            </Profiler>
         </HomePageContainer>
     )
 };
